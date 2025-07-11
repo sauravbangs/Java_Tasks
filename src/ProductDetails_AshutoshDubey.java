c7fd64685f6ed4316455fc9ceae57ea61a0783ad
ashutosh.dubey <ashutosh.dubey@bold.com>
2025-07-11 11:01:29 +0530

    Tasks: 1. Create a Java program named UserProfile_[yourName].java where you: Declare variables of different primitive types (int, double, char, boolean) Assign meaningful values (e.g., user age, salary, gender, isActive) Print these values using System.out.println() 2. Create a file named ProductDetails_[yourName].java where: You declare variables to store the product name, price, and quantity. Print the total value (price * quantity).

---
+++ src/ProductDetails_AshutoshDubey.java
@@ -0,0 +1,18 @@
+public class ProductDetails_AshutoshDubey {
+        public static void main(String[] args) {
+
+
+            String productName = "Airpods";
+            double price = 12999.99;
+            int quantity = 3;
+
+
+            double totalValue = price * quantity;
+
+
+            System.out.println("Product Name: " + productName);
+            System.out.println("Price per Unit: ₹" + price);
+            System.out.println("Quantity: " + quantity);
+            System.out.println("Total Value: ₹" + totalValue);
+        }
+}
---
+++ src/UserProfile_AshutoshDubey.java
@@ -0,0 +1,16 @@
+public class UserProfile_AshutoshDubey {
+    public static void main(String[] args) {
+        int age = 29;
+        double salary = 50000.99;
+        char gender = 'M';
+        boolean isActive = true;
+
+
+        System.out.println("User Profile:");
+        System.out.println("Name: Ashutosh Dubey");
+        System.out.println("Age: " + age);
+        System.out.println("Salary: ₹" + salary);
+        System.out.println("Gender: " + gender);
+        System.out.println("Active Status: " + isActive);
+    }
+}
