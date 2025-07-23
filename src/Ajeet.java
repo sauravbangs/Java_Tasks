d0d56b39ff308d8b8bb1ea4557461653a45be353
ajeetkumarbold <ajeet@bold.com>
2025-07-23 14:52:14 +0530

    first class

---
+++ src/Ajeet.java
@@ -0,0 +1,19 @@
+public class Ajeet {
+    public static void main(String[] args) {
+        // Declare variables of different primitive types
+        int userAge = 30;
+        double userSalary = 85000.50;
+        char gender = 'M';
+        boolean isActive = true;
+        // Print the values using System.out.println()
+
+
+        System.out.println("User Profile:");
+        System.out.println("Age: " + userAge);
+        System.out.println("Salary: $" + userSalary);
+        System.out.println("Gender: " + gender);
+        System.out.println("Active User: " + isActive);
+
+    }
+}
+
